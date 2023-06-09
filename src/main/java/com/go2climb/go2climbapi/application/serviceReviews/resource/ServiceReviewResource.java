6a9ea475de6cfa126aec55874becf720acc0347d
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2023-06-09 00:11:04 -0500

    (feat): ServiceReviews Add

---
+++ src/main/java/com/go2climb/go2climbapi/application/serviceReviews/resource/ServiceReviewResource.java
@@ -0,0 +1,21 @@
+package com.go2climb.go2climbapi.application.serviceReviews.resource;
+
+import com.go2climb.go2climbapi.application.tourists.resource.TouristResource;
+import com.go2climb.go2climbapi.application.services.resource.ServiceResource;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceReviewResource {
+    private Long id;
+    private String date;
+    private String comment;
+    private float score;
+    private ServiceResource service;
+    private TouristResource tourist;
+}
