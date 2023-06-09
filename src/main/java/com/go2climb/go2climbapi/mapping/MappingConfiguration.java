982b2448792af05d3ab37f3389655b49c69d5720
ErikRomeroPolli <U202019857@upc.edu.pe>
2023-06-08 23:56:11 -0500

    Project Create

---
+++ src/main/java/com/go2climb/go2climbapi/mapping/MappingConfiguration.java
@@ -0,0 +1,26 @@
+package com.go2climb.go2climbapi.mapping;
+
+import com.go2climb.go2climbapi.application.agencies.mapping.AgencyMapper;
+import com.go2climb.go2climbapi.application.agencyReviews.mapping.AgencyReviewMapper;
+import com.go2climb.go2climbapi.application.hiredServices.mapping.HiredServiceMapper;
+import com.go2climb.go2climbapi.application.serviceReviews.mapping.ServiceReviewMapper;
+import com.go2climb.go2climbapi.application.services.mapping.ServiceMapper;
+import com.go2climb.go2climbapi.application.tourists.mapping.TouristMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("Go2ClimbAPIMappingConfiguration")
+public class MappingConfiguration {
+    @Bean
+    public AgencyMapper AgencyMapper() { return new AgencyMapper(); }
+    @Bean
+    public HiredServiceMapper hiredServiceMapper() { return new HiredServiceMapper(); }
+    @Bean
+    public TouristMapper touristMapper() { return new TouristMapper(); }
+    @Bean
+    public ServiceMapper serviceMapper() { return new ServiceMapper(); }
+    @Bean
+    public AgencyReviewMapper agencyReviewMapper() { return new AgencyReviewMapper(); }
+    @Bean
+    public ServiceReviewMapper serviceReviewMapper() { return new ServiceReviewMapper(); }
+}
